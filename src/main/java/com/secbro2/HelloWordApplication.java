6b242063bce22518dbfa6b43013f96b81d3fc415
Molly Huang <molly.h@gmail.com>
2021-05-26 22:59:20 +0800

    #11. SpringBoot入口功能详解 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class) //@ComponentScan(basePackages = "com.controller") //@ComponentScan(resourcePattern = "../*.class")

--- src/main/java/com/secbro2/HelloWordApplication.java
+++ src/main/java/com/secbro2/HelloWordApplication.java
@@ -2,8 +2,12 @@ package com.secbro2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+//@ComponentScan(basePackages = "com.controller")
+//@ComponentScan(resourcePattern = "../*.class")
 public class HelloWordApplication {
 
 	public static void main(String[] args) {
