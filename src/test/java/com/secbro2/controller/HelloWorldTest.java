c3b8904539a7ab79fff1630983a3102a36b64ffa
Molly Huang <molly.h@gmail.com>
2021-05-26 16:14:01 +0800

    SpringBoot之HelloWorld及单元测试 - 比對回應 status code and content         mockMvc.perform(MockMvcRequestBuilders.get("/hello"))                 .andExpect(status().isOk())                 .andExpect(content().string(equalTo("Hello World!")));

--- src/test/java/com/secbro2/controller/HelloWorldTest.java
+++ src/test/java/com/secbro2/controller/HelloWorldTest.java
@@ -7,7 +7,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author Molly Huang
@@ -25,6 +28,9 @@ public class HelloWorldTest {
 
     @Test
     public void testHello() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/hello")).andDo(print());
+        //mockMvc.perform(MockMvcRequestBuilders.get("/hello")).andDo(print());
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World!")));
     }
 }
