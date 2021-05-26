7776e73c6814b7bf23504e6a4cb411712e4284b4
Molly Huang <molly.h@gmail.com>
2021-05-26 16:07:50 +0800

    SpringBoot之HelloWorld及单元测试 - Junit 測試 HelloWorldController

--- src/test/java/com/secbro2/controller/HelloWorldTest.java
+++ src/test/java/com/secbro2/controller/HelloWorldTest.java
@@ -1,15 +1,30 @@
 package com.secbro2.controller;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 /**
  * @author Molly Huang
  * 最簡單的單元測試 - 不需外部請求時
  */
+@SpringBootTest
 public class HelloWorldTest {
 
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
+    }
+
     @Test
-    public void testPrint() {
-        System.out.println("Hello World!");
+    public void testHello() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello")).andDo(print());
     }
 }
