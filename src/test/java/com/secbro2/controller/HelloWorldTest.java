39b97a57f95ea2c1c8bcbeea3326ebbee3a23745
Molly Huang <molly.h@gmail.com>
2021-05-26 16:36:55 +0800

    9. SpringBoot之MockMvc详解 - 更換另一種外部環境 WebApplicationContext 來測試

--- src/test/java/com/secbro2/controller/HelloWorldTest.java
+++ src/test/java/com/secbro2/controller/HelloWorldTest.java
@@ -2,11 +2,15 @@ package com.secbro2.controller;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -15,16 +19,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 /**
  * @author Molly Huang
- * 最簡單的單元測試 - 不需外部請求時
+ * 需要外部請求
  */
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class HelloWorldTest {
 
     private MockMvc mockMvc;
 
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
     @Before
     public void setup() {
-        mockMvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
+        //mockMvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
     @Test
