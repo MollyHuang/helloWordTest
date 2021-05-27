4c7d4c9bff58aa8b96066980e2e92c43f4e5c7d9
Molly Huang <molly.h@gmail.com>
2021-05-27 15:38:04 +0800

    105. Redis中Key的序列化
    com.secbro2.controller.RedisTest.java

--- src/test/java/com/secbro2/controller/RedisTest.java
+++ src/test/java/com/secbro2/controller/RedisTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import javax.annotation.Resource;
 
@@ -15,9 +19,35 @@ public class RedisTest {
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     @Test
-    public void testSimple() throws Exception {
-        redisTemplate.opsForValue().set("newWeb","google.com");
-        Assertions.assertEquals("google.com", redisTemplate.opsForValue().get("newWeb"));
+    void testSimple() {
+        redisTemplate.opsForValue().set("testSimple", "google.com");
+        Assertions.assertEquals("google.com", redisTemplate.opsForValue().get("testSimple"));
+    }
+
+    @Test
+    void testStringTemplate() {
+        ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
+        operations.set("testStringTemplate", "541075754");
+        Assertions.assertEquals("541075754", operations.get("testStringTemplate"));
+    }
+
+    @Test
+    void testKeySerializer() {
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        ValueOperations<String, Object> operations = redisTemplate.opsForValue();
+        operations.set("testKeySerializer", "google.com");
+        Assertions.assertEquals("google.com", operations.get("testKeySerializer"));
+    }
+
+    @Test
+    void testKeySerializer1() {
+        redisTemplate.setKeySerializer(new StringRedisSerializer());    // 結果同上，另一種序列化的方式
+        ValueOperations<String, Object> operations = redisTemplate.opsForValue();
+        operations.set("testKeySerializer1", "google.com");
+        Assertions.assertEquals("google.com", operations.get("testKeySerializer1"));
     }
 }
