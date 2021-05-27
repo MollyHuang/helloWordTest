55c8f9e7afcc05ff77460b04ef6c62c15feb7f91
Molly Huang <molly.h@gmail.com>
2021-05-27 15:18:48 +0800

    測試檔加入@Slf4j

--- src/test/java/com/secbro2/controller/RedisTest.java
+++ src/test/java/com/secbro2/controller/RedisTest.java
@@ -1,5 +1,6 @@
 package com.secbro2.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,6 +8,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 
 import javax.annotation.Resource;
 
+@Slf4j
 @SpringBootTest
 public class RedisTest {
 
