526e877fe20c80410c943b0647f729092dc70df8
Molly Huang <molly.h@gmail.com>
2021-05-27 16:16:22 +0800

    #106. Redis操作对象数据
    com.secbro2.controller.RedisObjectTest
    com.model.Order
    pom.xml - Add lombok dependency

---
+++ src/main/java/com/model/Order.java
@@ -0,0 +1,16 @@
+package com.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Order implements Serializable {
+
+    private int id;
+
+    private String orderNo;
+
+    private int amount;
+}
+
